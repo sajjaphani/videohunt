2d4c55f989a55bb7357c269c00136196c6f7c423
Nischal <nischal.srinivas@gmail.com>
2017-07-12 19:28:27 +0530

    sample

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import HeaderContainer from '../../containers/Header'
 import HomePage from '../HomePage'
@@ -10,17 +10,17 @@ import NotFoundPage from '../NotFoundPage'
 export default class App extends React.Component {
     render() {
         const mainStyle = {
-            marginTop: "5em"
+            marginTop: "5em" //my change
         }
+        const route='/'
         return (
             <div>
                 <HeaderContainer/>
                 <Container style={mainStyle}>
-                    <Switch>
                         <Route exact path="/" component={HomePage} />
                         <Route path="/new-post" component={AddPostPage} />
                         <Route component={NotFoundPage} />
-                    </Switch>
+                        <Redirect to={route}/>
                 </Container>
             </div>
         )
