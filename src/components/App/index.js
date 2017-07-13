8acf5f5aaa5747832af20a5d3bb0c27afbe3d814
Nischal <nischal.srinivas@gmail.com>
2017-07-13 09:10:10 +0530

    Fix connected router in App container

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
 import { Switch, Route } from 'react-router';
-import { push } from 'react-router-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import {history} from '../../store'
 
 import HeaderContainer from '../../containers/Header'
 import HomePage from '../HomePage'
@@ -17,15 +18,22 @@ const App = () => {
             <div>
                 <HeaderContainer />
                 <Container style={mainStyle}>
+                    <ConnectedRouter history={history}>
                         <Switch>
                             <Route exact path="/" component={HomePage} />
                             <Route path="/new-post" component={AddPostPage} />
                             <Route component={NotFoundPage} />
                         </Switch>
+                    </ConnectedRouter>
+
                 </Container>
             </div>
         )
     }
 }
+
+
+const Public = () => <h3>Public</h3>
+const Login = () => <h3>Login</h3>
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/App'
-import { ConnectedRouter } from 'react-router-redux'
 import { Provider } from 'react-redux'
-import { history, configureStore } from './store';
+import { configureStore } from './store';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <AppContainer />
-    </ConnectedRouter>
+    <AppContainer />
   </Provider>
   ,
   document.getElementById('app')
