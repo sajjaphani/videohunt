ca7dc9026837e5b08b35942adb894e289a51e719
Nischal <nischal.srinivas@gmail.com>
2017-08-06 16:31:22 +0530

    Add comments in mock data

--- src/api/sample2.js
+++ src/api/sample2.js
@@ -22,7 +22,7 @@ const samplejson = {
                 "userId": "595f3e5365ac7f15c7a92b9c",
                 "postedOn": "2017-07-25T06:52:07.859Z",
                 "likes": [],
-                "comments": [],
+                "comments": ['comment1', 'comment2', 'comment3'],
                 "id": "59784446c8cb59186c256852"
             },
             "597844acc8cb59186c256853": {
@@ -63,7 +63,7 @@ const samplejson = {
                 "postedOn": "2017-07-27T05:52:07.859Z",
                 "likes": [],
                 "comments": [
-                    
+                    'comment4', 'comment5', 'comment6'
                 ],
                 "id": "5979815a37552e2b24679d8c"
             },
@@ -80,11 +80,58 @@ const samplejson = {
         },
         "users": {
             "595f3e5365ac7f15c7a92b9c": {
+                "id": "595f3e5365ac7f15c7a92b9c",
                 "name": "Phani Sajja",
                 "headline": "Some Headline...",
                 "email": "sajja.phani@gmail.com",
-                "memberSince": "2017-07-07T07:54:59.864Z",
-                "id": "595f3e5365ac7f15c7a92b9c"
+                "memberSince": "2017-07-07T07:54:59.864Z"
+            },
+            "595f459865ac7f15c7a92b9d": {
+                "id": "595f459865ac7f15c7a92b9d",
+                "name": "Nischal Srinivas",
+                "headline": "Some Headline...",
+                "email": "nischal.srinivas@gmail.com",
+                "memberSince": "2017-07-07T08:26:00.723Z",
+                "picture": "https://scontent.xx.fbcdn.net/v/t1.0-1/c0.0.50.50/p50x50/19059568_10212456734831929_5171602577296152885_n.jpg?oh=bf54ffa8c51dd22687020ae061a151d8&oe=59F4FF15"
+            },
+            "595f45b565ac7f15c7a92b9e": {
+                "id": "595f45b565ac7f15c7a92b9e",
+                "name": "Kiran Kumar",
+                "headline": "Some Headline...",
+                "email": "kirankumar.iiit@gmail.com",
+                "memberSince": "2017-07-07T08:26:29.090Z"
+            }
+        },
+        "comments": {
+            "comment1": {
+                "id": "comment1",
+                "text": "This is a sample comment1",
+                "userId": "595f459865ac7f15c7a92b9d"
+            },
+            "comment2": {
+                "id": "comment2",
+                "text": "This is another comment2",
+                "userId": "595f3e5365ac7f15c7a92b9c"
+            },
+            "comment3": {
+                "id": "comment3",
+                "text": "This is a sample comment3",
+                "userId": "595f45b565ac7f15c7a92b9e"
+            },
+            "comment4": {
+                "id": "comment4",
+                "text": "This is another comment4",
+                "userId": "595f459865ac7f15c7a92b9d"
+            },
+            "comment5": {
+                "id": "comment5",
+                "text": "This is a sample comment5",
+                "userId": "595f45b565ac7f15c7a92b9e"
+            },
+            "comment6": {
+                "id": "comment6",
+                "text": "This is another comment6",
+                "userId": "595f459865ac7f15c7a92b9d"
             }
         }
     },
