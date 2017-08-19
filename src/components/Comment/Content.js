b6ea32a9e9b0958056d55fa8afd7c0f891b0e75c
Nischal <nischal.srinivas@gmail.com>
2017-08-19 19:29:48 +0530

    Dont show likes text if 0 likes

--- src/components/Comment/Content.js
+++ src/components/Comment/Content.js
@@ -5,12 +5,16 @@ import Emojify from 'react-emojione'
 
 const Content = (props) => {
     const { userName, commentText, commentedOn, userPicture, children, totalLikes } = props
-    const likeText = totalLikes == 1 ? 'like' : 'likes'
+    let likeText = ''
+    if (totalLikes > 0) {
+        const countText = '. '+ totalLikes
+        likeText = totalLikes == 1 ?  countText + ' like' : countText + ' likes'
+    }
     return (
             <Comment.Content>
                 <Comment.Author as='a'>{userName}</Comment.Author>
                 <Comment.Metadata>
-                    {moment(commentedOn).calendar()} . {totalLikes} {likeText}
+                    {moment(commentedOn).calendar()} {likeText}
                 </Comment.Metadata>
                 <Comment.Text>
                     <Emojify>
