0b1fa503acba50c745235901e1b18723612f5c83
Sajja Phani <sajja.phani@gmail.com>
2017-08-25 23:33:03 +0530

    Updated to handle getting video id from parameter

--- src/components/Post/Video.js
+++ src/components/Post/Video.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { Embed, Segment } from 'semantic-ui-react'
 
+function getParameter(url, name) {
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) 
+        return null;
+    if (!results[2]) 
+        return '';
+
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 const Video = props => {
-    const vidId = props.url.includes('v=') ? props.url.split('v=').pop() : ''
-    const placeholder = 'http://img.youtube.com/vi/' + vidId + '/maxresdefault.jpg'
+    const videoId = getParameter(props.url, 'v')
+    // console.log('V=', videoId)
+    // const vidId = props.url.includes('v=') ? props.url.split('v=').pop() : ''
+    // const placeholder = 'http://img.youtube.com/vi/' + vidId + '/maxresdefault.jpg'
+    const placeholder = 'https://i.ytimg.com/vi/' + videoId + '/sddefault.jpg'
+    if (videoId == '' || videoId == null)
+        return (<div />);
+
     return (
         <Segment attached clearing>
             <Embed
-                id={vidId}
+                id={videoId}
                 placeholder={placeholder}
                 source='youtube'
                 autoplay={true}
