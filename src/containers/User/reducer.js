6577971d7dbc9bdcd45025a2dad778f5f6060689
Sajja Phani <sajja.phani@gmail.com>
2017-09-02 17:01:42 +0530

    Updated to handle the users, comments in their corresponding reducers in load more videos success

--- src/containers/User/reducer.js
+++ src/containers/User/reducer.js
@@ -5,6 +5,7 @@ import * as ActionTypes from './constants'
 const userReducer = (state = fromJS({}), action) => {
     switch (action.type) {
         case ActionTypes.LOAD_VIDEOS_SUCCESS:
+        case ActionTypes.LOAD_MORE_VIDEOS_SUCCESS:
         case ActionTypes.LOAD_SINGLE_POST_SUCCESS:        
             return state.mergeDeep(action.payload.data.users)
         case ActionTypes.LOGIN_SUCCESS:
