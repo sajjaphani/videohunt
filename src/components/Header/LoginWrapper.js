3b4f6ba47e8ebe15cabf21561daaac664cb8873a
Sajja Phani <sajja.phani@gmail.com>
2017-09-12 18:07:39 +0530

    Added notifications icon

--- src/components/Header/LoginWrapper.js
+++ src/components/Header/LoginWrapper.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import UserAccount from './UserAccount'
 import LoginButton from './LoginButton'
+import Notifications from './Notifications'
 
 const LoginWrapper = props => {
     const { isLoggedIn, loginLoading, handleLogout, loginModalOpen, openLogin, handleProfile, handleSettings } = props
     if (isLoggedIn) {
-        return <UserAccount userId={props.userId} handleLogout={handleLogout} handleProfile={handleProfile} handleSettings={handleSettings}/>
+        return <div>
+            <Notifications />
+            <UserAccount userId={props.userId} handleLogout={handleLogout} handleProfile={handleProfile} handleSettings={handleSettings} />
+        </div>
     } else {
         return <LoginButton openLogin={openLogin} open={loginModalOpen} handleLogin={props.handleLogin} loginLoading={loginLoading} />
     }
