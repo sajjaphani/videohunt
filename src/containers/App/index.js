adfad5710b9a1ef3febd7aa3671312e62bd0ae2e
Sajja Phani <sajja.phani@gmail.com>
2020-01-17 10:30:46 +0530

    removed DB unused

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,9 +1,6 @@
 import React from "react";
-// import { Helmet } from 'react-helmet';
 
 import AppContainer from './app-container'
-
-/*global FB*/
 
 class App extends React.Component {
 
@@ -13,14 +10,6 @@ class App extends React.Component {
 
     render() {
         return (
-            // <div className="app">
-            //     <Helmet>
-            //         <title>VideoHunt</title>
-            //         <meta charSet="utf-8" />
-            //         <meta name="description" content="VideoHunt App" data-react-helmet="true" />
-            //         <meta name="keywords" content="videohunt,video,video aggregation" data-react-helmet="true" />
-            //     </Helmet>
-            // </div>
             <AppContainer />
         );
     }
